c5f540dfb6b7082e1a0acdc1b4d29c6409163fd5
kmatusiak <konr2303@gmail.com>
2016-01-14 16:34:32 +0100

    Serwer komunikując się z agentem mnoży dwie macierze Serwer przesyła kolejne tablice do pomnożenia Klient zwraca wynik mnożenia

--- Serwer/client.c
+++ Serwer/client.c
@@ -6,27 +6,32 @@
 #include <unistd.h>
 
 #define MAX_SIZE 256
+#define readI(fd, x) read(fd, x, sizeof(int))
+#define readF(fd, x) read(fd, x, sizeof(float))
+#define writeI(fd, x) write(fd, x, sizeof(int))
+#define writeF(fd, x) write(fd, x, sizeof(float))
 
 float* readRow(int fd, int len){
     static float *row  = NULL;
-    //row = malloc(sizeof(float) * len);
     row = (float *)calloc( len , sizeof(calloc) );
-    //memset(row, 0, sizeof(float) * len);
     read(fd, row, sizeof(float) * len);
     if (row == NULL){
         printf("Error");
     }
-	int i;
-	for(i = 0; i < len; i++){
-        printf("%f\n", *(row + i));
-	}
     return row;
 }
 
+float multiply(float* row, float* column, int len){
+    float result;
+    result = 0;
+    int i;
+    for(i = 0; i < len; i++){
+        result += (*(row + i) * *(column + i));
+    }
+    return result;
+}
+
 int main(int argc, char** argv) {
-    //gniazdo strumieniowe - TCP - sock_stream
-    //gniazko datagramowe - UDP - sock_dgam
-    //socket zwraca deskryptor do pliku
 	int fd, n;
     char buf[256];
     struct sockaddr_in sa;
@@ -34,25 +39,31 @@ int main(int argc, char** argv) {
 	float f[2];
     fd = socket(PF_INET, SOCK_STREAM, 0);
 	addrent=gethostbyname("localhost");
-    //socket address - socket
     sa.sin_family = PF_INET;
-    //konwersja portu 80 z repr. lokalnej do sieciowej do 16 bitow
     sa.sin_port = htons(1234);
-    //inet_addr tlumaczy XX.XX.XX.XX na ciag 32 bitow
-    //sa.sin_addr.s_addr = inet_addr(argv[1]);
 	memcpy(&sa.sin_addr.s_addr, addrent->h_addr, addrent->h_length);
     connect(fd, (struct sockaddr*)&sa, sizeof(sa));
-	//write(fd, argv[1], 6);
-	int len;
-	n = read(fd, &len, sizeof(len));
-	float *rowA, *rowB;
-	rowA = readRow(fd, len);
-    rowB = readRow(fd, len);
+
+    int len, rowsCount;
+    readI(fd, &len);
+    readI(fd, &rowsCount);
+    printf("Rows:%d Columns:%d\n", rowsCount, len);
     int i;
-    for(i = 0; i < len; i++){
-        printf("%f\n", *(rowB + i));
-	}
-	printf("%d\n", len);
+    for(i = 0; i < rowsCount * len; i++){
+        float *first, *second;
+        int x, y;
+        readI(fd, &x);
+        readI(fd, &y);
+        printf("Row:%d Column:%d ", x, y);
+        first = readRow(fd, len);
+        second = readRow(fd, len);
+        float result = multiply(first, second, len);
+        printf("Value:%f\n", result);
+        writeI(fd, &x);
+        writeI(fd, &y);
+        writeF(fd, &result);
+    }
+
     close(fd);
     return 0;
 }
--- Serwer/server.c
+++ Serwer/server.c
@@ -6,30 +6,27 @@
 #include <unistd.h>
 #include <signal.h>
 
-//SERWER: socket() -> bind()-> listen() -> accept() -> write()/read() -> close() ->accept()
+#define readI(fd, x) read(fd, x, sizeof(int))
+#define readF(fd, x) read(fd, x, sizeof(float))
+#define writeI(fd, x) write(fd, x, sizeof(int))
+#define writeF(fd, x) write(fd, x, sizeof(float))
+
 void childend()
 {
     wait(NULL);
-    printf("*** End of child process\n");
+    printf("Koniec połączenia\n");
 }
 
 
 int main(int argc, char** argv) {
-    //gniazdo strumieniowe - TCP - sock_stream
-    //gniazko datagramowe - UDP - sock_dgam
-    //socket zwraca deskryptor do pliku
     int fd, n , b,l,fd2,size, on=1;
-    char buf[6],buf2[6]={1,1,7,1,9,3};
     struct sockaddr_in sa;
     struct sockaddr_in caddr[atoi(argv[1])];
 	signal(SIGCHLD, childend);
     fd = socket(PF_INET, SOCK_STREAM, 0);
 	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
-    //socket address - socket
     sa.sin_family = PF_INET;
-    //konwersja portu 80 z repr. lokalnej do sieciowej do 16 bitow
     sa.sin_port = htons(1234);
-    //inet_addr tlumaczy XX.XX.XX.XX na ciag 32 bitow
     sa.sin_addr.s_addr = INADDR_ANY;
 
     b=bind(fd, (struct sockaddr*)&sa, sizeof(sa));
@@ -39,16 +36,32 @@ int main(int argc, char** argv) {
         fd2=accept(fd, (struct sockaddr*)&caddr[0], &size);
         if(!fork()){
             close(fd);
-            printf("nowe polaczenie: %s:%i\n", inet_ntoa((struct in_addr)caddr[0].sin_addr), caddr[0].sin_port);
-            float tab[3] = {1.0, 2.0, 3};
-            int i;
-            int x;
-            x = sizeof(tab)/sizeof(float);
-            write(fd2, &x, sizeof(int));
-            write(fd2, tab, sizeof(tab));
-            write(fd2, tab, sizeof(tab));
-            printf("%d\n", x);
+            printf("Nowe połączenie: %s:%i\n", inet_ntoa((struct in_addr)caddr[0].sin_addr), caddr[0].sin_port);
+            float matrixA[2][2] = {{2, 2},{2, 2}};
+            float matrixB[2][2] = {{2, 2},{2, 2}};
+            int rowsCount = sizeof(matrixA)/sizeof(matrixA[0]);
+            int len = sizeof(matrixA[0])/sizeof(float);
+            printf("Rows:%d Columns:%d\n", rowsCount, len);
+
+            writeI(fd2, &len);
+            writeI(fd2, &rowsCount);
+            int i, j;
+            for(i = 0; i < rowsCount; i++){
+                for(j = 0; j < len; j++){
+                    writeI(fd2, &i);
+                    writeI(fd2, &j);
+                    write(fd2, matrixA[i], sizeof(matrixA[0]));
+                    write(fd2, matrixB[j], sizeof(matrixB[0]));
+                    float result;
+                    int x, y;
+                    readI(fd2, &x);
+                    readI(fd2, &y);
+                    readF(fd2, &result);
+                    printf("Row:%d Column:%d Value:%f\n", x, y, result);
+                }
+            }
             write(fd2, "\n", 1);
+
             close(fd2);
             return 0;
         }
