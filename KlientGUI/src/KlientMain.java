048b6a1e0e9b7fdd2fa5f4addedc4fd11921a052
mpanowicz <michal.panowicz@student.put.poznan.pl>
2016-01-17 20:35:19 +0100

    Wylaczenie okna z wyborem typu klienta

--- KlientGUI/src/KlientMain.java
+++ KlientGUI/src/KlientMain.java
@@ -3,8 +3,10 @@ public class KlientMain {
 
 	public static void main(String[] args) {
 		try {
-			ChooseOption chooseOption = new ChooseOption();
-			chooseOption.open();
+			//ChooseOption chooseOption = new ChooseOption();
+			//chooseOption.open();
+			MainWindow window = new MainWindow();
+			window.open();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
