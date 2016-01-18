504375cf1d14de155b002f32728462067dcca1a4
mpanowicz <michal.panowicz@student.put.poznan.pl>
2016-01-18 02:27:27 +0100

    Dodane przesylanie drugiego pliku

--- KlientGUI/src/Comunication.java
+++ KlientGUI/src/Comunication.java
@@ -11,7 +11,7 @@ public class Comunication {
     	
         try {
 
-    		Socket socket = new Socket("192.168.0.100", 1234);
+    		Socket socket = new Socket("192.168.0.100", 4321);
             DataInputStream input = new DataInputStream(socket.getInputStream());
             PrintStream output = new PrintStream(socket.getOutputStream());
         	File file = new File("macierzA.txt");
@@ -25,6 +25,23 @@ public class Comunication {
 	            	}
 	            	output.print("\n");
 	        	}
+	        	output.print("e");
+	        }
+	        output = new PrintStream(socket.getOutputStream());
+	        file = new File("macierzB.txt");
+	        fileService = new FileService();
+	        matrix =  new ArrayList<List<BigDecimal>>();
+	        if(fileService.readFileIntoArray(file)){
+	        	fileService.transpose();
+	        	matrix = fileService.matrix;
+	        	
+	        	for(Iterator<List<BigDecimal>> i = matrix.iterator(); i.hasNext();){
+	            	for(Iterator<BigDecimal> j = i.next().iterator(); j.hasNext();){
+	            		output.print(j.next().toPlainString() + " ");
+	            	}
+	            	output.print("\n");
+	        	}
+	        	output.close();
 	        }
 	        
 	        socket.close();
