279c3e1cf2f6c59c0ec809644ceb5dfbf829d5f7
mpanowicz <michal.panowicz@student.put.poznan.pl>
2016-01-18 23:45:09 +0100

    Usuniecie zbednych zmiennych

--- Serwer/client.c
+++ Serwer/client.c
@@ -4,6 +4,9 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_SIZE 256
 #define readI(fd, x) read(fd, x, sizeof(int))
@@ -15,6 +18,11 @@ float* readRow(int fd, int len){
     static float *row  = NULL;
     row = (float *)calloc( len , sizeof(calloc) );
     read(fd, row, sizeof(float) * len);
+	int i;
+	for(i = 0; i < len; i++){
+		printf("%f ", row[i]);
+	}
+	printf("\n");
     if (row == NULL){
         printf("Error");
     }
@@ -32,38 +40,38 @@ float multiply(float* row, float* column, int len){
 }
 
 int main(int argc, char** argv) {
-	int fd, n;
-    char buf[256];
-    struct sockaddr_in sa;
-	struct hostent* addrent;
-	float f[2];
-    fd = socket(PF_INET, SOCK_STREAM, 0);
-	addrent=gethostbyname("localhost");
-    sa.sin_family = PF_INET;
-    sa.sin_port = htons(1234);
-	memcpy(&sa.sin_addr.s_addr, addrent->h_addr, addrent->h_length);
-    connect(fd, (struct sockaddr*)&sa, sizeof(sa));
-
-    int len, rowsCount;
-    readI(fd, &len);
-    readI(fd, &rowsCount);
-    printf("Rows:%d Columns:%d\n", rowsCount, len);
-    int i;
-    for(i = 0; i < rowsCount * len; i++){
-        float *first, *second;
-        int x, y;
-        readI(fd, &x);
-        readI(fd, &y);
-        printf("Row:%d Column:%d ", x, y);
-        first = readRow(fd, len);
-        second = readRow(fd, len);
-        float result = multiply(first, second, len);
-        printf("Value:%f\n", result);
-        writeI(fd, &x);
-        writeI(fd, &y);
-        writeF(fd, &result);
-    }
-
-    close(fd);
+	while(1){
+		int fd, con = 0;
+		struct sockaddr_in sa;
+		struct hostent* addrent;
+		fd = socket(PF_INET, SOCK_STREAM, 0);
+		addrent=gethostbyname("localhost");
+		sa.sin_family = PF_INET;
+		sa.sin_port = htons(1234);
+		memcpy(&sa.sin_addr.s_addr, addrent->h_addr, addrent->h_length);
+		con = connect(fd, (struct sockaddr*)&sa, sizeof(sa));
+		if(con == 0){
+			int len, rowsCount;
+			readI(fd, &len);
+			readI(fd, &rowsCount);
+			printf("Rows:%d Columns:%d\n", rowsCount, len);
+			int i;
+			for(i = 0; i < rowsCount * len; i++){
+				float *first, *second;
+				int x, y;
+				readI(fd, &x);
+				readI(fd, &y);
+				printf("Row:%d Column:%d \n", x, y);
+				first = readRow(fd, len);
+				second = readRow(fd, len);
+				float result = multiply(first, second, len);
+				printf("Value:%f\n", result);
+				writeI(fd, &x);
+				writeI(fd, &y);
+				writeF(fd, &result);
+			}
+		}
+		close(fd);
+	}
     return 0;
 }
