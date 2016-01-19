b5f49116d2386843694962b78b8f94e88b41a625
mpanowicz <michal.panowicz@student.put.poznan.pl>
2016-01-19 01:41:32 +0100

    Usuniecie zbednych komentarzy

--- KlientGUI/src/KlientMain.java
+++ KlientGUI/src/KlientMain.java
@@ -3,8 +3,6 @@ public class KlientMain {
 
 	public static void main(String[] args) {
 		try {
-			//ChooseOption chooseOption = new ChooseOption();
-			//chooseOption.open();
 			MainWindow window = new MainWindow();
 			window.open();
 		} catch (Exception e) {
