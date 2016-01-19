b63144883c674fdf4cbcc90d1700a57592e67a56
mpanowicz <michal.panowicz@student.put.poznan.pl>
2016-01-19 01:39:20 +0100

    Wyodrebnienie funkcji connect, send i read

--- KlientGUI/src/Comunication.java
+++ KlientGUI/src/Comunication.java
@@ -7,50 +7,66 @@ import java.math.BigDecimal;
 
 public class Comunication {
 
-    public static void main(String[] argv) {
-    	
+    public void connect(String address, Integer port, String matrixA, String matrixB, String matrixC){
         try {
 
-    		Socket socket = new Socket("192.168.0.100", 4321);
+    		Socket socket = new Socket(address, port);
             DataInputStream input = new DataInputStream(socket.getInputStream());
             PrintStream output = new PrintStream(socket.getOutputStream());
-        	File file = new File("macierzA.txt");
-	        FileService fileService = new FileService();
-	        List<List<BigDecimal>> matrix =  new ArrayList<List<BigDecimal>>();
-	        if(fileService.readFileIntoArray(file)){
-	        	matrix = fileService.matrix;
-	        	for(Iterator<List<BigDecimal>> i = matrix.iterator(); i.hasNext();){
-	            	for(Iterator<BigDecimal> j = i.next().iterator(); j.hasNext();){
-	            		output.print(j.next().toPlainString() + " ");
-	            	}
-	            	output.print("\n");
-	        	}
-	        	output.print("e");
-	        }
-	        output = new PrintStream(socket.getOutputStream());
-	        file = new File("macierzB.txt");
-	        fileService = new FileService();
-	        matrix =  new ArrayList<List<BigDecimal>>();
-	        if(fileService.readFileIntoArray(file)){
-	        	fileService.transpose();
-	        	matrix = fileService.matrix;
-	        	
-	        	for(Iterator<List<BigDecimal>> i = matrix.iterator(); i.hasNext();){
-	            	for(Iterator<BigDecimal> j = i.next().iterator(); j.hasNext();){
-	            		output.print(j.next().toPlainString() + " ");
-	            	}
-	            	output.print("\n");
-	        	}
-	        	output.close();
-	        }
-	        
+            sendFile(output, matrixA, false);
+            sendFile(output, matrixB, true);
+            readFile(input, matrixC);
 	        socket.close();
 
         } catch (UnknownHostException e) {
             System.out.println("Unkonw exception " + e.getMessage());
+            Error error = new Error();
+            error.open(e.getMessage());
 
         } catch (IOException e) {
             System.out.println("IOException caught " + e.getMessage());
+            Error error = new Error();
+            error.open(e.getMessage());
         } 
+    }
+    
+    private void sendFile(PrintStream output, String path, Boolean transpose){
+    	File file = new File(path);
+        FileService fileService = new FileService();
+        List<List<BigDecimal>> matrix =  new ArrayList<List<BigDecimal>>();
+        if(fileService.readFileIntoArray(file)){
+        	if(transpose){
+        		fileService.transpose();
+        	}
+        	matrix = fileService.matrix;
+        	for(Iterator<List<BigDecimal>> i = matrix.iterator(); i.hasNext();){
+            	for(Iterator<BigDecimal> j = i.next().iterator(); j.hasNext();){
+            		output.print(j.next().toPlainString() + " ");
+            	}
+            	output.print("\n");
+        	}
+        	output.print("e");
+        }
+    }
+    
+    @SuppressWarnings("deprecation")
+	private void readFile(DataInputStream input, String path){
+    	PrintWriter matrixC;
+		try {
+			matrixC = new PrintWriter(path);
+			String line = "";
+	        while((line = input.readLine()) != null && line.length() > 0){
+	        	matrixC.println(line);
+	        }
+	        matrixC.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			Error error = new Error();
+            error.open(e.getMessage());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			Error error = new Error();
+            error.open(e.getMessage());
+		}
     }
 }
